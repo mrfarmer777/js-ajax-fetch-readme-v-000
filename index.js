ee9c1265436d1fae747dacc72de3e8f82466f07e
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 19:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much.";
 
 
 function getRepos(){
-  const token = ''
+  const token = '3a993896292a1951c00e088811ec867de9ca741d';
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
