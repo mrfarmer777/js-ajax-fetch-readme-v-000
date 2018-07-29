d66900574d9b75d62ab9c4063738b707f7a262a6
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 19:23:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+
+
+function getRepos(){
+  const token = '3a993896292a1951c00e088811ec867de9ca741d';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+}
